e5ed2f18b422c729a01c939dc8ac6b82c22e9617
bing8u <676659348@qq.com>
2017-12-06 15:53:23 +0800

    设置defaultStringLength

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Encore\Admin\Config\Config;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Config::load();
+        Schema::defaultStringLength(250);
+//        Config::load();
     }
 
     /**
