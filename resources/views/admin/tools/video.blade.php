4f2155ef22cc175954556275a35d6bcb7a4c3b10
System Administrator <root@bingMacBook-Pro.local>
2018-01-20 15:02:08 +0800

    完成

--- resources/views/admin/tools/video.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="btn-group" data-toggle="buttons">
-    @foreach($options as $option => $label)
-        <label class="btn btn-default btn-sm {{ \Request::get('type', 'film') == $option ? 'active' : '' }}">
-            <input type="radio" class="video-type" value="{{ $option }}">{{$label}}
-        </label>
-    @endforeach
-</div>
--- resources/views/www/home/nav.blade.php
+++
@@ -1,6 +0,0 @@
-<nav class="nav">
-    <a class="nav-link {{ Route::is('home.travel*') || Route::is('home') ? 'active' : ''}}" href="{{ route('home.travel.index') }}">我的游记</a>
-    <a class="nav-link {{ Route::is('home.message') ? 'active' : ''}}" href="{{ route('home.message') }}">我的消息 @if(auth()->user()->messages()->where('read', false)->count()) <i class="fa fa-bell-o text-warning"></i> @endif</a>
-    <a class="nav-link {{ Route::is('home.order*') ? 'active' : ''}}" href="{{ url('home/order') }}">我的订单</a>
-    <a class="nav-link {{ Route::is('home.setting') ? 'active' : ''}}" href="{{ route('home.setting') }}">设置中心</a>
-</nav>
